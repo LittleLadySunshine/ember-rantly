f5fc6c490c1431e787013a268bb7e2eee6b8bb6c
Denise McCoy <Denise.L.McCoy@gmail.com>
2015-03-25 09:24:16 -0600

    index

--- app/controllers/rant/index.js
+++ app/controllers/rant/index.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
   isEditing: false,
+  needs: ['application', 'rant'],
+  sortProperties : ['createdAt'],
+  sortAscending  : false,
+  itemController: 'rant',
 
   actions: {
     editRant: function(){
