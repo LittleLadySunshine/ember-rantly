69e3a775de80bde26432da85f7257bfa65928c22
Denise McCoy <Denise.L.McCoy@gmail.com>
2015-03-29 16:51:01 -0600

    heroku deploy

--- app/controllers/rants/index.js
+++ app/controllers/rants/index.js
@@ -1,8 +1,6 @@
 import Ember from "ember";
 
 export default Ember.ArrayController.extend({
-  // this is to get a new rant to go to the top
-
   needs: ['application', 'rant'],
 
   sortProperties: ['createdAt'],
